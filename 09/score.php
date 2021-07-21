188d9df7cdc54394c8f36d3b87d838208e1fc2b0
arihito <arihito.m@gmail.com>
2021-07-21 18:40:24 +0900

    fix: 09 score

--- 09/score.php
+++ 09/score.php
@@ -1,18 +1,48 @@
 <?php
-$score = 120;
 
-if (!is_numeric($score)) {
-    echo '数値を入力してください';
-} elseif ($score > 100 || $score < 0) {
-    echo '不正な点数です';
-} else {
-    if ($score == 100) {
-        echo '満点おめでとう！';
-    } elseif ($score >= 80) {
-        echo '素晴らしいです！';
-    } elseif ($score >= 60) {
-        echo '合格です';
+$score = '';
+
+if (!empty($_POST)) {
+    $score = $_POST['score'];
+
+    if (!is_numeric($score)) {
+        $result = '数値を入力してください';
+    } elseif ($score > 100 || $score < 0) {
+        $result = '不正な点数です';
     } else {
-        echo '不合格です';
+        if ($score == 100) {
+            $result = '満点おめでとう！';
+        } elseif ($score >= 80) {
+            $result = '素晴らしいです！';
+        } elseif ($score >= 60) {
+            $result = '合格です';
+        } else {
+            $result = '不合格です';
+        }
     }
 }
+?>
+<!DOCTYPE html>
+<html lang="ja">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>テスト結果判定</title>
+</head>
+
+<body>
+    <h1>テスト結果判定</h1>
+    <form action="" method="post">
+        <p>点数：
+            <input type="text" name="score" size="3" maxlength="3" value="<?=htmlspecialchars($score, ENT_QUOTES, 'UTF-8');?>">点
+            <input type="submit" value="判定">
+        </p>
+        <?php if ($_SERVER["REQUEST_METHOD"] === "POST"): ?>
+            <p>判定：<?=htmlspecialchars($result, ENT_QUOTES, 'UTF-8');?></p>
+        <?php endif; ?>
+    </form>
+</body>
+
+</html>
