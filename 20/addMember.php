bf7290f8de103730e615f9f1ff07ec229f8f7e43
arihito <arihito.m@gmail.com>
2021-08-05 10:34:35 +0900

    hotfix: 20 addMember validationFlag

--- 20/addMember.php
+++ 20/addMember.php
@@ -16,7 +16,7 @@ if (!empty($_POST)) {
     if ($name === '') {
         $nameError = '※ 氏名を入力してください';
         $isValidated = false;
-    } elseif (mb_strlen($age) > 10) {
+    } elseif (mb_strlen($name) > 10) {
         $nameError = '※ 氏名を10文字以内で入力してください';
         $isValidated = false;
     }
@@ -24,7 +24,7 @@ if (!empty($_POST)) {
     if ($age === '') {
         $age = null;
     } elseif (!is_numeric($age) || $age < 0) {
-        $nameError = '※ 年齢は0以上の数値を入力してください';
+        $ageError = '※ 年齢は0以上の数値を入力してください';
         $isValidated = false;
     }
 
@@ -59,15 +59,34 @@ if (!empty($_POST)) {
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>会員管理システム</title>
+    <style>
+        .error {
+            color: #900;
+            font-size: 14px;
+            font-weight: bold;
+        }
+    </style>
 </head>
 
 <body>
     <h1>会員登録</h1>
     <p><a href="member.php">会員一覧に戻る</a></p>
     <form action="" method="post">
-        <p>氏名：<input type="text" name="name" value="<?= h($name) ?>"></p>
-        <p>年齢：<input type="text" name="age" value="<?= h($age) ?>"></p>
-        <p>住所：<input type="text" name="address" value="<?= h($address) ?>"></p>
+        <p>
+            氏名：<input type="text" name="name" value="<?= h($name) ?>">
+            <?php if (isset($nameError)) : ?>
+                <span class="error"><?= h($nameError) ?></span>
+            <?php endif; ?>
+        </p>
+        <p>
+            年齢：<input type="text" name="age" value="<?= h($age) ?>">
+            <?php if (isset($ageError)) : ?>
+                <span class="error"><?= h($ageError) ?></span>
+            <?php endif; ?>
+        </p>
+        <p>
+            住所：<input type="text" name="address" value="<?= h($address) ?>">
+        </p>
         <p><input type="submit" value="送信"></p>
     </form>
 </body>
