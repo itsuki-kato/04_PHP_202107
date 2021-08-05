2f71a85251fecdc9e47bed5f3c52cfa0f9020d4d
arihito <arihito.m@gmail.com>
2021-08-05 10:30:42 +0900

    fix: 20 addMember validationFlag

--- 20/addMember.php
+++ 20/addMember.php
@@ -5,30 +5,49 @@ require_once 'util.inc.php';
 $name = '';
 $age = '';
 $address = '';
+$isValidated = false;
 
 if (!empty($_POST)) {
+    $isValidated = true;
     $name = $_POST['name'];
     $age = $_POST['age'];
     $address = $_POST['address'];
 
-    try {
-        $pdo = db_init();
-        // $sql = 'INSERT INTO members (name, age, address, created_at) VALUES (?, ?, ?, NOW())';
-        // $stmt = $pdo->prepare($sql);
-        // $stmt->execute([$name, $age, $address]);
-        $sql = 'INSERT INTO members (name, age, address, created_at)
-                VALUES (:name, :age, :address, NOW())';
-        $stmt = $pdo->prepare($sql);
-        $stmt->bindValue(':name',    $name,    PDO::PARAM_STR);
-        $stmt->bindValue(':age',     $age,     PDO::PARAM_INT);
-        $stmt->bindValue(':address', $address, PDO::PARAM_STR);
-        $stmt->execute();
+    if ($name === '') {
+        $nameError = '※ 氏名を入力してください';
+        $isValidated = false;
+    } elseif (mb_strlen($age) > 10) {
+        $nameError = '※ 氏名を10文字以内で入力してください';
+        $isValidated = false;
+    }
 
-        header('Location: member.php');
-        exit;
-    } catch (PDOException $e) {
-        header('Content-Type: text/plain; charset=UTF-8', true, 500);
-        exit($e -> getMessage());
+    if ($age === '') {
+        $age = null;
+    } elseif (!is_numeric($age) || $age < 0) {
+        $nameError = '※ 年齢は0以上の数値を入力してください';
+        $isValidated = false;
+    }
+
+    if ($isValidated == true) {
+        try {
+            $pdo = db_init();
+            // $sql = 'INSERT INTO members (name, age, address, created_at) VALUES (?, ?, ?, NOW())';
+            // $stmt = $pdo->prepare($sql);
+            // $stmt->execute([$name, $age, $address]);
+            $sql = 'INSERT INTO members (name, age, address, created_at)
+                VALUES (:name, :age, :address, NOW())';
+            $stmt = $pdo->prepare($sql);
+            $stmt->bindValue(':name',    $name,    PDO::PARAM_STR);
+            $stmt->bindValue(':age',     $age,     PDO::PARAM_INT);
+            $stmt->bindValue(':address', $address, PDO::PARAM_STR);
+            $stmt->execute();
+
+            header('Location: member.php');
+            exit;
+        } catch (PDOException $e) {
+            header('Content-Type: text/plain; charset=UTF-8', true, 500);
+            exit($e->getMessage());
+        }
     }
 }
 ?>
@@ -46,9 +65,9 @@ if (!empty($_POST)) {
     <h1>会員登録</h1>
     <p><a href="member.php">会員一覧に戻る</a></p>
     <form action="" method="post">
-        <p>氏名：<input type="text" name="name" value="<?=h($name)?>"></p>
-        <p>年齢：<input type="text" name="age" value="<?=h($age)?>"></p>
-        <p>住所：<input type="text" name="address" value="<?=h($address)?>"></p>
+        <p>氏名：<input type="text" name="name" value="<?= h($name) ?>"></p>
+        <p>年齢：<input type="text" name="age" value="<?= h($age) ?>"></p>
+        <p>住所：<input type="text" name="address" value="<?= h($address) ?>"></p>
         <p><input type="submit" value="送信"></p>
     </form>
 </body>
